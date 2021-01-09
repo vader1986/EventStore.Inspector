891de2a69091978ad559fc2b04155fcfd195b643
Timm Hoffmeister <timm.hoffmeister@gmx.de>
2021-01-09 20:13:53 +0000

    Added Search test for no events found for search options

--- EventStore.Inspector.Common.Tests/Search/SearchTests.cs
+++ EventStore.Inspector.Common.Tests/Search/SearchTests.cs
@@ -12,38 +12,56 @@ namespace EventStore.Inspector.Common.Tests.Search
     [TestFixture]
     public class SearchTests
     {
+        private static readonly EventWrapperBuilder AnEvent = new EventWrapperBuilder().WithEventData(new { id = "ABC-123", text = "my text" });
+
         private IOutputStream _outputStream;
+        private ThrottleOptions _throttleOptions;
+        private SearchOptions _searchOptions;
 
         [SetUp]
         public void Before()
         {
             _outputStream = A.Fake<IOutputStream>();
+            _searchOptions = new SearchOptions(new[] { new TextFilter("my text") }, AnEvent.Build().Stream, OutputFormat.Text, AggregationMethod.And);
+            _throttleOptions = new ThrottleOptions(BatchMode.Continue, 0);
         }
 
         [Test]
-        public async Task For_existing_event_writes_result_to_output_stream()
+        public async Task For_search_options_matching_an_event_writes_result_to_output_stream()
         {
-            var wrapper = new EventWrapperBuilder()
-                .WithEventData(new { id = "ABC-123", text = "my text" })
-                .Build();
+            var @event = AnEvent.Build();
+            using var eventStore = await EventStoreRunner.Start(@event.Stream);
 
-            using var eventStore = await EventStoreRunner.Start(wrapper.Stream);
+            await eventStore.Write(@event);
 
-            await eventStore.Write(wrapper);
-
-            var throttleOptions = new ThrottleOptions(BatchMode.Continue, 0);
-            var options = new ConnectionOptions(eventStore.Connection, true, 50, throttleOptions);
-
-            var filter = new ISearchFilter[] { new TextFilter("my text") };
-            var searchOptions = new SearchOptions(filter, wrapper.Stream, OutputFormat.Text, AggregationMethod.And);
+            var options = new ConnectionOptions(eventStore.Connection, true, 50, _throttleOptions);
 
             await SearchBuilder
                 .From(options)
                 .WithOutputStream(_outputStream)
                 .Build()
-                .For(searchOptions);
+                .For(_searchOptions);
 
             A.CallTo(() => _outputStream.Append(A<string>.Ignored)).MustHaveHappenedOnceExactly();
+        }
+
+        [Test]
+        public async Task For_search_options_not_matching_any_events_does_not_write_to_output_stream()
+        {
+            var @event = AnEvent.WithEventData(new { ignore = "this" }).Build();
+            using var eventStore = await EventStoreRunner.Start(@event.Stream);
+
+            await eventStore.Write(@event);
+
+            var options = new ConnectionOptions(eventStore.Connection, true, 50, _throttleOptions);
+
+            await SearchBuilder
+                .From(options)
+                .WithOutputStream(_outputStream)
+                .Build()
+                .For(_searchOptions);
+
+            A.CallTo(() => _outputStream.Append(A<string>.Ignored)).MustNotHaveHappened();
         }
     }
 }
